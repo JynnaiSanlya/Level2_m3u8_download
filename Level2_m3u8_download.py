36794d62f061687ef2b67d1ffe15a88c6f4ffcf6
JynnaiSanlya <154421935+JynnaiSanlya@users.noreply.github.com>
2024-05-06 22:17:48 +0800

    Update Level2_m3u8_download.py
    Init.

--- Level2_m3u8_download.py
+++ Level2_m3u8_download.py
@@ -21,6 +21,49 @@ def combine(ts_path, combine_path, file_name):
             print(i)
     print('合并完成')
 
+# 从m3u8端口持续获取切片.ts文件
+# 
+def get_ts(url_m3u8,header):
+    d_m3u8 = requests.get(url_m3u8,header).content.decode('utf-8')   # 访问网页获取返回信息
+    ts_list = re.findall(r'(.*?).ts', d_m3u8)     # 正则提取出列表
+    return ts_list
+
+# 获取并且导出切片文件到本地
+def dow_ts(aim,url,path):
+    # for 循环拼接成完整的url下载链接
+    for i in range(0,len(url)):
+        url[i] = aim +'.'+ url[i] +'.ts'
+    x = 0 # 用于给多个视频进行子等命名
+    print(url)
+    # for循环取数据进行下载
+    for url_l in url:
+        # 设置保存的地址
+        path1 = path + str(x).zfill(3) + '.ts'
+        # path1 = path + str(x)+ '.ts'
+        try:
+            print(url_l)  # 用于展示下载的是哪个视频链接
+            vi = requests.get(url_l,timeout=5).content   # 获取到下载链接的返回值
+            with open(path1,'wb') as mp4:   # 下载视频到本地
+                mp4.write(vi)
+            print(str(x) +'下载成功')
+            x = x + 1
+        except:
+            print('下载失败')
+
+            # 将已经下载的ts文件的路径进行排序
+def file_walker(path):
+    # 建一个空列表用来放文件夹里面的文件
+    file_list = []
+    # for循环把文件夹中的文件输出为绝对路径的列表file_list
+    for root, dirs, files in os.walk(path): # 生成器
+        # 对列表中的数据进行排序
+        files.sort()
+        # for 为了把files列表中的数据改造成指定的类型
+        for i in range(0,len(files)):
+            files[i] = v_fi1 +files[i]
+        file_list = files
+    return file_list
+
 def checkFileExist(filenameAbsolute):
     if not os.path.exists(filenameAbsolute):
         with open(filenameAbsolute, 'w'):
